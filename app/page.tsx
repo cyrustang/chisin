3b9cf2fa170e97aebf64aaf34730010d1ffe73ce
cyrustang <cyrus@cyrustang.com>
2024-10-19 14:50:46 +0800

    +

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import dynamic from 'next/dynamic'
+
+const OpusDeiComponent = dynamic(() => import('@/components/opus-dei'), {
+  ssr: false
+})
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <OpusDeiComponent />
+    </main>
+  )
+}
